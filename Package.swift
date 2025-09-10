739681f50acc5e8eef9fe1957bab334d271943cf
ukit-tokyo <t.yuki@ukit.tokyo>
2025-09-10 21:14:39 +0900

    add swift-format

--- Package.swift
+++ Package.swift
@@ -4,24 +4,26 @@
 import PackageDescription
 
 let package = Package(
-    name: "DeepLinkOrganizer",
-    platforms: [
-        .iOS(.v17)
-    ],
-    products: [
-        // Products define the executables and libraries a package produces, making them visible to other packages.
-        .library(
-            name: "DeepLinkOrganizer",
-            targets: ["DeepLinkOrganizer"])
-    ],
-    targets: [
-        // Targets are the basic building blocks of a package, defining a module or a test suite.
-        // Targets can depend on other targets in this package and products from dependencies.
-        .target(
-            name: "DeepLinkOrganizer"),
-        .testTarget(
-            name: "DeepLinkOrganizerTests",
-            dependencies: ["DeepLinkOrganizer"]
-        ),
-    ]
+  name: "DeepLinkOrganizer",
+  platforms: [
+    .iOS(.v17)
+  ],
+  products: [
+    // Products define the executables and libraries a package produces, making them visible to other packages.
+    .library(
+      name: "DeepLinkOrganizer",
+      targets: ["DeepLinkOrganizer"]
+    )
+  ],
+  targets: [
+    // Targets are the basic building blocks of a package, defining a module or a test suite.
+    // Targets can depend on other targets in this package and products from dependencies.
+    .target(
+      name: "DeepLinkOrganizer"
+    ),
+    .testTarget(
+      name: "DeepLinkOrganizerTests",
+      dependencies: ["DeepLinkOrganizer"]
+    ),
+  ]
 )
